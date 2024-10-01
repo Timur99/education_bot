0c3e2e37caf35b72c5d7044ade93b3edaee273bb
Тимур Сайфуллин <tsayfullin@webpower.ru>
2024-10-01 13:47:14 +0300

    v1

--- tg_v1.py
+++ tg_v1.py
@@ -1,6 +1,6 @@
 from scriptss.llm_script import get_tasks
 
-objects = input('Введи предмет, по которому хочешь полуить задачу: ' )
-rang = input('Какой уровень сложности: ' )
+objects = 'python' #input('Введи предмет, по которому хочешь полуить задачу: ' )
+rang = 'easy' #input('Какой уровень сложности: ' )
 
 get_tasks(objects, rang)
